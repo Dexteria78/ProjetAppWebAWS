4329602d570d0a2a958966c2b3ed460455bb8e97
Dexteria78 <nicolasguerin@outlook.fr>
2026-02-19 05:06:18 +0100

    fix: update ECS task health check to use port 3000

--- phase6/terraform/main.tf
+++ phase6/terraform/main.tf
@@ -415,7 +415,7 @@ resource "aws_ecs_task_definition" "app" {
     }
 
     healthCheck = {
-      command     = ["CMD-SHELL", "curl -f http://localhost/ || exit 1"]
+      command     = ["CMD-SHELL", "curl -f http://localhost:3000/ || exit 1"]
       interval    = 30
       timeout     = 5
       retries     = 3
